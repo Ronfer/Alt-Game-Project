4ab34a14295f78152bf28d0c1ffb8f7ac53ecad7
Ronnie Isomaki <roni.isomaki@gmail.com>
2019-07-05 12:33:47 +0300

    Commit

---
+++ Alt-Game/Scenes/Player/PlayerController.gd
@@ -0,0 +1,63 @@
+extends KinematicBody2D
+
+
+const MOTION_SPEED = 140 # Pixels/second
+
+func _ready():
+	$AnimatedSprite.play("IdleDown")
+
+
+func _process(_delta):
+	var motion = Vector2()
+	
+	if Input.is_action_pressed("move_up"):
+		motion += Vector2(0, -1)
+		$AnimatedSprite.play("WalkUp")
+	if Input.is_action_just_released("move_up"):
+        $AnimatedSprite.play("IdleUp")
+		
+	if Input.is_action_pressed("move_down"):
+		$AnimatedSprite.play("WalkDown")
+		motion += Vector2(0, 1)
+	if Input.is_action_just_released("move_down"):
+        $AnimatedSprite.play("IdleDown")
+
+	if Input.is_action_pressed("move_left"):
+		$AnimatedSprite.play("WalkLeft")
+		motion += Vector2(-1, 0)
+	if Input.is_action_just_released("move_left"):
+        $AnimatedSprite.play("IdleLeft")
+
+	if Input.is_action_pressed("move_right"):
+		$AnimatedSprite.play("WalkRight")
+		motion += Vector2(1, 0)
+	if Input.is_action_just_released("move_right"):
+        $AnimatedSprite.play("IdleRight")
+
+	if Input.is_action_pressed("move_up_left"):
+		$AnimatedSprite.play("WalkLeftUp")
+		motion += Vector2(-1 ,-1)
+	if Input.is_action_just_released("move_up_left"):
+		$AnimatedSprite.play("IdleLeftUp")
+	
+	if Input.is_action_pressed("move_up_right"):
+		$AnimatedSprite.play("WalkRightUp")
+		motion += Vector2(1 ,-1)
+	if Input.is_action_just_released("move_up_right"):
+		$AnimatedSprite.play("IdleRightUp")
+
+	if Input.is_action_pressed("move_down_left"):
+		$AnimatedSprite.play("WalkLeftDown")
+		motion += Vector2(-1 ,1)
+	if Input.is_action_just_released("move_down_left"):
+		$AnimatedSprite.play("IdleLeftDown")
+
+	if Input.is_action_pressed("move_down_right"):
+		$AnimatedSprite.play("WalkDownRight")
+		motion += Vector2(1, 1)
+	if Input.is_action_just_released("move_down_right"):
+		$AnimatedSprite.play("IdleRightDown")
+
+	motion = motion.normalized() * MOTION_SPEED
+
+	move_and_slide(motion)
