ef2a8671bbfc8210ccfa5db27e439d8d4bc0fd42
Ronnie Isomaki <roni.isomaki@gmail.com>
2019-07-13 11:18:48 +0300

    commit

--- Alt-Game/Scenes/Player/PlayerController.gd
+++ Alt-Game/Scenes/Player/PlayerController.gd
@@ -1,9 +1,13 @@
 extends KinematicBody2D
 
+#SpawnPoints
+export var speed = 140 # Pixels/second
 
-const MOTION_SPEED = 140 # Pixels/second
+func _init():
+	randomize()
 
 func _ready():
+	self.position = get_node("/root/Node2D/SpawnPoints").get_child(randi() % 3).position
 	$AnimatedSprite.play("IdleDown")
 
 
@@ -36,28 +40,28 @@ func _process(_delta):
 
 	if Input.is_action_pressed("move_up_left"):
 		$AnimatedSprite.play("WalkLeftUp")
-		motion += Vector2(-1 ,-1)
+		motion += Vector2(-2 ,-1)
 	if Input.is_action_just_released("move_up_left"):
 		$AnimatedSprite.play("IdleLeftUp")
 	
 	if Input.is_action_pressed("move_up_right"):
 		$AnimatedSprite.play("WalkRightUp")
-		motion += Vector2(1 ,-1)
+		motion += Vector2(2 ,-1)
 	if Input.is_action_just_released("move_up_right"):
 		$AnimatedSprite.play("IdleRightUp")
 
 	if Input.is_action_pressed("move_down_left"):
 		$AnimatedSprite.play("WalkLeftDown")
-		motion += Vector2(-1 ,1)
+		motion += Vector2(-2 ,1)
 	if Input.is_action_just_released("move_down_left"):
 		$AnimatedSprite.play("IdleLeftDown")
 
 	if Input.is_action_pressed("move_down_right"):
 		$AnimatedSprite.play("WalkDownRight")
-		motion += Vector2(1, 1)
+		motion += Vector2(2, 1)
 	if Input.is_action_just_released("move_down_right"):
 		$AnimatedSprite.play("IdleRightDown")
 
-	motion = motion.normalized() * MOTION_SPEED
+	motion = motion.normalized() * speed
 
 	move_and_slide(motion)
